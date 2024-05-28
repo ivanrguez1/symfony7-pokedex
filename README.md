a00139c32f566ae75163cdda9fd151f173950f77
Iván Rodríguez <ivan.rodriguez@soltelsevilla.onmicrosoft.com>
2024-05-28 20:47:32 +0200

    [#] Clase 2024-05-28

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# symfony7-pokedex
+# Pokedex
+
+## 1. Crear y borrar BBDD
+
+```console
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+php bin/console doctrine:migrations:migrate
+```
+- drop -> Eliminas la bbdd
+- create -> Creas la bbdd
+- migrate -> Introduces tablas
+
+## Endpoints (direcciones)
+- localhost:8000/categorias/insertar
+- localhost:8000/categorias/insertar/semilla
+- localhost:8000/categorias/insertar-array
---
+++ src/Controller/CategoriasController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorias;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/categorias', name: 'app_categorias_')]
+class CategoriasController extends AbstractController
+{
+    #[Route('/index', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('categorias/index.html.twig', [
+            'controller_name' => 'CategoriasController',
+        ]);
+    }
+
+    #[Route('/insertar', name: 'insertar')]
+    public function insertar(ManagerRegistry $doctrine): Response
+    {
+        $gestorEntidades = $doctrine->getManager();
+        $categoria = new Categorias();
+        $categoria->setCategoria("Llama");
+
+        $gestorEntidades->persist($categoria);
+        $gestorEntidades->flush();
+
+        return new Response("Categoria insertada con ID" .
+            $categoria->getId());
+    }
+
+    #[Route('/insertar/{categoria}', name: 'insertarParam')]
+    public function insertarParam(
+        EntityManagerInterface $gestorEntidades,
+        string $categoria
+    ): Response {
+        $nuevaCategoria = new Categorias();
+        $nuevaCategoria->setCategoria($categoria);
+
+        $gestorEntidades->persist($nuevaCategoria);
+        $gestorEntidades->flush();
+
+        return new Response("Categoria insertada con ID" .
+            $nuevaCategoria->getId());
+    }
+
+    #[Route('/insertar-array', name: 'insertarArray')]
+    public function insertarArray(
+        EntityManagerInterface $gestorEntidades
+    ): Response {
+        $categorias = array(
+            'categoria1' => array(
+                'nombre' => 'Lagartija'
+            ),
+            'categoria2' => array(
+                'nombre' => 'Mariposa'
+            ),
+            'categoria3' => array(
+                'nombre' => 'Capullo'
+            ),
+        );
+
+        foreach ($categorias as $categoria) {
+            $nuevaCategoria = new Categorias();
+            $nuevaCategoria->setCategoria($categoria['nombre']);
+
+            $gestorEntidades->persist($nuevaCategoria);
+            $gestorEntidades->flush();
+        }
+
+        return new Response("Categorias insertadas");
+    }
+}
